6c09029f48fb555f965c4c41fbf50ce942492773
David Chaparro <dchaparro0723@gmail.com>
2022-08-02 18:40:01 -0300

    Ejercicio1

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/* 
+Se usa para comentarios de varias lineas
+* Este es un comentario de una linea
+Este es otro comentario de una linea
+*/
+
+
+// Este va a ser el mensaje de salida
+console.log("NOS VEMOOOOOOOSSSSSSSSS");
